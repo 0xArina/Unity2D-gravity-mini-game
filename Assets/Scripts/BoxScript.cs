a121150554f925a315b1e1e0914cd24cad9e3797
Arina <43216874+rereshek@users.noreply.github.com>
2020-01-24 19:11:16 -0500

    Add files via upload

---
+++ Assets/Scripts/BoxScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BoxScript : MonoBehaviour {
+
+  
+    Vector2 getGravity;     
+        
+    // Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //LMB pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            getGravity = Physics2D.gravity;
+            Physics2D.gravity = Vector2.zero;
+        }
+        //LMB released
+        if (Input.GetMouseButtonUp(0))
+        {
+            Physics2D.gravity = getGravity;
+        }
+       
+        if (Input.GetMouseButton(0)){
+             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            if (hit.collider != null)
+            {
+              hit.collider.gameObject.transform.position = hit.point;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/gravityTextScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gravityTextScript : MonoBehaviour {
+
+    public Text gravityText;
+
+    // Use this for initialization
+    void Start () {
+        gravityText = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gravityText.text = "gravity off";
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            gravityText.text = "gravity on";
+        }
+    }
+}
